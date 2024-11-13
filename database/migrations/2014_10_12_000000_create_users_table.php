9dd0e027fce114bfe561a206cce368b033b7013f
moshtaqAlwtr <moshtaqkaid6@gmail.com>
2024-11-13 19:21:11 +0300

    init

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('frist_name');
-            $table->string('last_name');
+            $table->string('name');
+
 
             $table->string('email')->unique();
 
--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,27 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-// routes/web.php
-
-
 use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+use App\Http\Controllers\ProfileController;
 
+
+
+// عرض تسجيل الدخول كصفحة رئيسية للزوار
+Route::get('/', function () {
+    return redirect()->route('login');
+})->middleware('guest');
+
+// مجموعة المسارات باستخدام تعدد اللغات
 Route::group(
     [
         'prefix' => LaravelLocalization::setLocale(),
-        'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
+        'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath']
     ],
     function () {
-        // الصفحة الرئيسية
-        Route::get('/', function () {
+        Route::get('/welcome', function () {
             return view('welcome');
-        });
+        })->name('home');
 
-        // مسارات الصفحات الأخرى
         Route::get('/human-resources', function () {
             return view('layouts.nav-slider-route', ['page' => 'human_resources']);
         })->name('human_resources');
@@ -39,4 +32,15 @@ Route::group(
     }
 );
 
+// مسارات المصادقة
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
